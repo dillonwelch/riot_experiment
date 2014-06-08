a0239c50f1aa002d995234d8fda26a94632c1dd3
Dillon Welch <daw0328@gmail.com>
2014-06-07 22:28:15 -0400

    Moved cURL code into separate class.

--- includes/curl.php
+++ includes/curl.php
@@ -1,4 +1,55 @@
 <?php
 /**
- * Helper functions for making cURL calls.
+ * Helper class for making cURL calls to Riot API.
  */
+
+class CURLHelper {
+
+  // The base URL of all API calls.
+  const BASE_API_URL = 'https://na.api.pvp.net/api/lol/';
+
+  // API key that needs to be passed along with all calls.
+  const API_KEY = '25f78534-b9f9-43de-93c4-a371dad3066c';
+
+  const CERT_LOCATION = '/cert/riot_cert.cer';
+
+  private $curl_instance;
+
+  private $url;
+
+  public function __construct() {
+    $this->curl_instance = curl_init();
+    $this->url = self::BASE_API_URL . 'na/v1.4/summoner/by-name/BumblingBear?api_key=' . self::API_KEY;
+  }
+
+  public function make_api_call() {
+    $this->set_curl_options();
+    $this->set_ssl_settings();
+
+    $result = curl_exec($this->curl_instance);
+    curl_close($this->curl_instance);
+
+    return $result;
+  }
+
+  private function set_curl_options() {
+    curl_setopt($this->curl_instance, CURLOPT_URL, $this->url);
+    curl_setopt($this->curl_instance, CURLOPT_HEADER, 0);
+    curl_setopt($this->curl_instance, CURLOPT_RETURNTRANSFER, 1);
+  }
+
+  /**
+   * Sets up SSL settings to be able to read from Riot's HTTPS API servers.
+   */
+  private function set_ssl_settings() {
+    // Verify that the server cert is valid.
+    curl_setopt($this->curl_instance, CURLOPT_SSL_VERIFYPEER, true);
+
+    // Check that the common name exists and it matches the server's host name>
+    curl_setopt($this->curl_instance, CURLOPT_SSL_VERIFYHOST, 2);
+
+    // Get the CA info from the cert downloaded from a Riot API results page.
+    curl_setopt($this->curl_instance, CURLOPT_CAINFO, getcwd() . self::CERT_LOCATION);
+  }
+
+}
--- index.php
+++ index.php
@@ -1,41 +1,15 @@
 <?php
 
-// The base URL of all API calls.
-define('BASE_API_URL', 'https://na.api.pvp.net/api/lol/');
+require_once '/includes/curl.php';
 
-define('API_KEY', '25f78534-b9f9-43de-93c4-a371dad3066c');
 
 echo "Hey there!<br>";
 
-$url = BASE_API_URL . 'na/v1.4/summoner/by-name/BumblingBear?api_key=' . API_KEY;
+$curl_helper = new CURLHelper();
 
-echo $url . "\n";
-
-$curl = curl_init();
-
-curl_setopt($curl, CURLOPT_URL, $url);
-curl_setopt($curl, CURLOPT_HEADER, 0);
-//curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false); //this works!
-curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, true);
-curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 2);
-curl_setopt($curl, CURLOPT_CAINFO, getcwd() . '/cert/riot_cert.cer');
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-
-$result = curl_exec($curl);
+$result = $curl_helper->make_api_call();
 
 echo '<br><br><br><br>';
-
-echo var_dump(curl_getinfo($curl));
-
-echo '<br><br>';
-
-echo curl_error($curl);
-
-echo '<br><br>';
-
-echo curl_errno($curl);
-
-echo '<br><br>';
 
 echo count($result);
 
@@ -45,14 +19,6 @@ echo var_dump($result);
 
 echo '<br><br>';
 
-
-curl_close($curl);
-
-//echo $result;
-
-echo "<br>That was fun.";
-
-
-
+echo "<br>That was fun.<br>";
 
 echo '"This product is not endorsed, certified or otherwise approved in any way by Riot Games, Inc. or any of its affiliates."';
